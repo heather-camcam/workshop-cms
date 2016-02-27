80a4f99d357ca94aab24ce6c2f7a7f169dc50948
Heather <campbell.hf@gmail.com>
2016-02-27 16:43:44 +0000

    Split our handler into a separate module

--- server.js
+++ server.js
@@ -1,56 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var querystring = require('querystring');
-
-function handler (request, response) {
-
-  var method = request.method;
-
-    var url = request.url;
-
-    console.log(method, url);
-
-
-    if (url === "/") {
-      response.writeHead(200, {"Content-Type": "text/html"});
-      fs.readFile(__dirname + '/assets/index.html', function(error, file) {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        response.end(file);
-      });
-    }
-    else if(url === '/create-post'){
-    response.writeHead(307, {"Location": '/' });
-      var allTheData = '';
-      request.on('data', function (chunkOfData) {
-
-        allTheData += chunkOfData;
-      });
-
-      request.on('end', function () {
-
-        var convertedData = querystring.parse(allTheData);
-        console.log(convertedData);
-        response.end();
-      });
-
-    } else {
-      fs.readFile(__dirname + '/assets' + url, function(error, file) {
-        if (error) {
-          console.log(error);
-          return;
-        } else {
-          var extension = url.split('.').pop();
-          response.writeHead(200, {"Content-Type": "text/" + extension});
-          response.end(file);
-        }
-      });
-    }
-}
-
-var server = http.createServer(handler);
+var handlerModule = require('./src/handler.js');
+var server = http.createServer(handlerModule);
 
 server.listen(3000, function() {
   console.log("Server is listening on port 3000. Ready to accept accept!");

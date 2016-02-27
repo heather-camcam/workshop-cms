6176c9622a4a9d1d7a7918c62da864698242517f
Em01 <emma_williams1@hotmail.co.uk>
2016-02-27 15:14:56 +0000

    Renaming

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function handler (request, response) {
 
     if (url === "/") {
       response.writeHead(200, {"Content-Type": "text/html"});
-      fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      fs.readFile(__dirname + '/assets/index.html', function(error, file) {
         if (error) {
           console.log(error);
           return;

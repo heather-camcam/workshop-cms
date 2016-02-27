703745b46d61620105f6163f2cd6f878415b980c
Em01 <emma_williams1@hotmail.co.uk>
2016-02-27 16:48:16 +0000

    Clear out handler and server

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var querystring = require('querystring');
-var handlerModule = require('./src/handler.js');
-var server = http.createServer(handlerModule);
-
-server.listen(3000, function() {
-  console.log("Server is listening on port 3000. Ready to accept accept!");
-});
